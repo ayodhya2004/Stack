480bd8c9e223b3cb8cb156291a91efbaf60ad456
ayodhya2004 <ashiniayodhya3@gmail.com>
2024-06-06 21:19:11 +0530

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,62 @@
+package stack;
+
+
+public class Stack {
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public Stack(int size) {
+        maxSize = size;
+        stackArray = new int[maxSize];
+        top = -1;
+    }
+
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push value.");
+            return;
+        }
+        stackArray[++top] = value;
+    }
+
+ 
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop value.");
+            return -1;
+        }
+        return stackArray[top--];
+    }
+
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot peek value.");
+            return -1;
+        }
+        return stackArray[top];
+    }
+
+ 
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Top element is: " + stack.peek());
+        System.out.println("Popped element is: " + stack.pop());
+        System.out.println("Top element is: " + stack.peek());
+    }
+}
